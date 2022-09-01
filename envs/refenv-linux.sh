647f9a082d54d5032f668f9c03058ddb4b3bbc42
Simon A. F. Lund <os@safl.dk>
2022-09-02 00:23:41 +0200

    A complete re-implementation and change of LICENSE
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- envs/refenv-linux.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#
-# Consult the different modules for the environment variables that they make use of
-#
---
+++ src/cijoe/linux/worklets/build_kdebs.py
@@ -0,0 +1,47 @@
+"""
+Linux Custom Kernel as Debian Package
+=====================================
+
+There are a myriad of ways to build and install a custom Linux kernel. This worklet
+builds it as a Debian package. The generated .deb packages are stored in
+cijoe.output_path.
+
+Retagetable: true
+-----------------
+
+Worklet arguments
+-----------------
+
+with.localversion
+"""
+from pathlib import Path
+
+
+def worklet_entry(args, cijoe, step):
+    """Configure, build and collect the build-artifacts"""
+
+    repos = Path(cijoe.config.options["repository"]["path"]).resolve()
+    err, _ = cijoe.run(f"[ -d {repos} ]")
+    if err:
+        return err
+
+    localversion = step.get("with", {"localversion": "custom"}).get("localversion")
+
+    commands = [
+        "[ -f .config ] && rm .config || true",
+        "yes " " | make olddefconfig",
+        "./scripts/config --disable CONFIG_DEBUG_INFO",
+        "./scripts/config --disable SYSTEM_TRUSTED_KEYS",
+        "./scripts/config --disable SYSTEM_REVOCATION_KEYS",
+        f"yes '' | make -j$(nproc) bindeb-pkg LOCALVERSION={localversion}",
+        f"mkdir -p {cijoe.output_path}/artifacts/linux",
+        f"mv ../*.deb {cijoe.output_path}/artifacts/linux",
+        f"mv ../*.changes {cijoe.output_path}/artifacts/linux",
+        f"mv ../*.buildinfo {cijoe.output_path}/artifacts/linux",
+    ]
+    for cmd in commands:
+        err, _ = cijoe.run(cmd, cwd=str(repos))
+        if err:
+            return err
+
+    return 0
