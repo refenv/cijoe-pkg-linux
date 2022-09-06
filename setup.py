ab41da65e8f8e4bcd92c50afed3e438d71fd12be
Simon A. F. Lund <os@safl.dk>
2022-09-06 21:07:03 +0200

    ver: bump to v0.9.1
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="cijoe-pkg-linux",
-    version="0.9.0",
+    version="0.9.1",
     author="Simon A. F. Lund",
     author_email="os@safl.dk",
     url="https://github.com/refenv/cijoe-pkg-linux/",
